985f5f22259b2d5b2787bd46e9e3f04c68b7696f
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-22 22:06:38 -0300

    adicionado id openModal e youtubeIframe e javascript para aparicao do trailer

---
+++ main.js
@@ -0,0 +1,7 @@
+const modalContainer = document.querySelector("#modalContainer");
+const openModal = document.querySelector("#openModal");
+const youtubeIframe = document.querySelector("#youtubeIframe");
+
+openModal.addEventListener("click", () =>{
+    modalContainer.style.display = "block";
+})
